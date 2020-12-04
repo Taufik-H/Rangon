26ebc887fa03a3c9580306a86a9392f14c0bde6f
Taufik <taufikrpl1@gmail.com>
2020-12-04 12:55:07 +0700

    move the rangon

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pygame.math import Vector2
 # class RANGON:
 #     def __init__(self):
 #         self.body = []
-# membuat class makanan & reactangle makanan
+# membuat class makanan & rectangle makanan
 
 class MAKANAN:
     def __init__(self):
@@ -16,6 +16,25 @@ class MAKANAN:
     def gambar_makanan(self):
         makanan_rect = pygame.Rect(int(self.pos.x * ukuran_cell), int(self.pos.y * ukuran_cell), ukuran_cell, ukuran_cell)
         pygame.draw.rect(screen, (214, 103, 103), makanan_rect)
+
+# membuat ular & rectangle
+
+class RANGON:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10),  Vector2(7, 10)]
+        self.direction = Vector2(1,0)
+    def gambar_rangon(self):
+        for block in self.body:
+            x_pos = int(block.x * ukuran_cell)
+            y_pos = int(block.y * ukuran_cell)
+            block_rect = pygame.Rect(x_pos, y_pos, ukuran_cell, no_cell)
+            pygame.draw.rect(screen, (72, 150, 189), block_rect)
+               
+    # menggerakan rangon
+    def move_rangon(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0,body_copy[0] + self.direction)
+        self.body = body_copy[:]
 
 
 # DISPLAY
@@ -27,15 +46,22 @@ pygame.display.set_caption('RANGON')
 clock = pygame.time.Clock()
 
 makanan = MAKANAN()
+rangon = RANGON()
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            rangon.move_rangon()
 
-    screen.fill((87, 212, 180))
+    screen.fill((103, 214, 146))
     makanan.gambar_makanan()
+    rangon.gambar_rangon()
     pygame.display.update()
 
     clock.tick(60)
