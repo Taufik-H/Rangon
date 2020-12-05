73ebbf24a87365a7502ca8da5a86c3bc1d7a28f4
Taufik <taufikrpl1@gmail.com>
2020-12-05 14:56:32 +0700

    adding asset

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class MAKANAN:
         self.pindah_makanan()
     def gambar_makanan(self):
         makanan_rect = pygame.Rect(int(self.pos.x * ukuran_cell), int(self.pos.y * ukuran_cell), ukuran_cell, ukuran_cell)
-        pygame.draw.rect(screen, (214, 103, 103), makanan_rect)
+        screen.blit(food, makanan_rect)
     def pindah_makanan(self):
         self.x = random.randint(0, no_cell - 1)
         self.y = random.randint(0, no_cell - 1)
@@ -23,9 +23,10 @@ class MAKANAN:
 
 class RANGON:
     def __init__(self):
-        self.body = [Vector2(5, 10), Vector2(6, 10),  Vector2(7, 10)]
+        self.body = [Vector2(5, 10), Vector2(4, 10),  Vector2(3, 10)]
         self.direction = Vector2(1,0)
         self.tambah_ngaceng = False
+
     def gambar_rangon(self):
         for block in self.body:
             x_pos = int(block.x * ukuran_cell)
@@ -56,15 +57,26 @@ class MAIN:
     def update(self):
         self.rangon.move_rangon()
         self.nyepong()
+        self.check_fail()
     def draw_element(self):
         self.makanan.gambar_makanan()
         self.rangon.gambar_rangon()
-
+    def check_fail(self):
+        # jika rangon menabrak screen maka quit window
+        if not 0 <= self.rangon.body[0].x < no_cell or not 0 <= self.rangon.body[0].y < no_cell:
+            self.game_over()
+        # jika menabrak body dari rangon maka quit
+        for block in self.rangon.body[1:]:
+            if block == self.rangon.body[0]:
+                self.game_over()
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
         # kalo rangon nyepong makanan 
         # maka makanan akan berpindah & 
         # tambahkakn blok pada rangon(tambah ngatjeng)
     def nyepong(self):
-        if self.makanan.pos == self.rangon.body[0]:
+        if  self.makanan.pos == self.rangon.body[0]:
             self.makanan.pindah_makanan()
             self.rangon.ngaceng()
 
@@ -75,7 +87,7 @@ no_cell = 26
 screen = pygame.display.set_mode((no_cell * ukuran_cell, no_cell * ukuran_cell))
 pygame.display.set_caption('RANGON')
 clock = pygame.time.Clock()
-
+food = pygame.image.load('assets/food.png').convert_alpha()
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
@@ -91,13 +103,17 @@ while True:
             # rangon keymap 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w or event.key == pygame.K_UP:
-                main_game.rangon.direction = Vector2(0, -1)
+                if main_game.rangon.direction.y !=1:
+                    main_game.rangon.direction = Vector2(0, -1)
             if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                 main_game.rangon.direction = Vector2(-1,0)
+                if main_game.rangon.direction.x != 1:
+                    main_game.rangon.direction = Vector2(-1,0)
             if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                 main_game.rangon.direction = Vector2(0, 1)
+                if main_game.rangon.direction.y != -1:
+                    main_game.rangon.direction = Vector2(0, 1)
             if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                main_game.rangon.direction = Vector2(1, 0)
+                if main_game.rangon.direction.x !=-1:
+                    main_game.rangon.direction = Vector2(1, 0)
 
     screen.fill((103, 214, 146))
     main_game.draw_element()
