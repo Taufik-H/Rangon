632938d551d67cf3fc9f5b383d09493b46f768a2
Taufik <taufikrpl1@gmail.com>
2020-12-05 09:36:08 +0700

    penambahan dan randomize blok

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ from pygame.math import Vector2
 
 class MAKANAN:
     def __init__(self):
-        self.x = random.randint(0, no_cell - 1)
-        self.y = random.randint(0, no_cell - 1)
-        self.pos = Vector2(self.x, self.y)
+        self.pindah_makanan()
     def gambar_makanan(self):
         makanan_rect = pygame.Rect(int(self.pos.x * ukuran_cell), int(self.pos.y * ukuran_cell), ukuran_cell, ukuran_cell)
         pygame.draw.rect(screen, (214, 103, 103), makanan_rect)
+    def pindah_makanan(self):
+        self.x = random.randint(0, no_cell - 1)
+        self.y = random.randint(0, no_cell - 1)
+        self.pos = Vector2(self.x, self.y)
 
 # membuat ular & rectangle
 
@@ -23,6 +25,7 @@ class RANGON:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10),  Vector2(7, 10)]
         self.direction = Vector2(1,0)
+        self.tambah_ngaceng = False
     def gambar_rangon(self):
         for block in self.body:
             x_pos = int(block.x * ukuran_cell)
@@ -32,10 +35,38 @@ class RANGON:
                
     # menggerakan rangon
     def move_rangon(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0,body_copy[0] + self.direction)
-        self.body = body_copy[:]
 
+        if self.tambah_ngaceng == True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.tambah_ngaceng = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0,body_copy[0] + self.direction)
+            self.body = body_copy[:]
+    def ngaceng(self):
+        self.tambah_ngaceng = True
+
+# class main
+class MAIN:
+    def __init__(self):
+        self.rangon = RANGON()
+        self.makanan = MAKANAN()
+    def update(self):
+        self.rangon.move_rangon()
+        self.nyepong()
+    def draw_element(self):
+        self.makanan.gambar_makanan()
+        self.rangon.gambar_rangon()
+
+        # kalo rangon nyepong makanan 
+        # maka makanan akan berpindah & 
+        # tambahkakn blok pada rangon(tambah ngatjeng)
+    def nyepong(self):
+        if self.makanan.pos == self.rangon.body[0]:
+            self.makanan.pindah_makanan()
+            self.rangon.ngaceng()
 
 # DISPLAY
 pygame.init()
@@ -45,32 +76,31 @@ screen = pygame.display.set_mode((no_cell * ukuran_cell, no_cell * ukuran_cell))
 pygame.display.set_caption('RANGON')
 clock = pygame.time.Clock()
 
-makanan = MAKANAN()
-rangon = RANGON()
+
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
 
-
+main_game = MAIN()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == SCREEN_UPDATE:
-            rangon.move_rangon()
+           main_game.update()
+            # rangon keymap 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w or event.key == pygame.K_UP:
-                 rangon.direction = Vector2(0,-1)
+                main_game.rangon.direction = Vector2(0, -1)
             if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                 rangon.direction = Vector2(-1,0)
+                 main_game.rangon.direction = Vector2(-1,0)
             if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                 rangon.direction = Vector2(0, 1)
+                 main_game.rangon.direction = Vector2(0, 1)
             if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                 rangon.direction = Vector2(1, 0)
+                main_game.rangon.direction = Vector2(1, 0)
 
     screen.fill((103, 214, 146))
-    makanan.gambar_makanan()
-    rangon.gambar_rangon()
+    main_game.draw_element()
     pygame.display.update()
 
     clock.tick(60)
