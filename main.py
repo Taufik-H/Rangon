a3f7b676690e47e4d3c95f51e295e17e1d853389
Taufik <taufikrpl1@gmail.com>
2020-12-07 10:20:24 +0700

    done grafik & fixing display

--- main.py
+++ main.py
@@ -44,14 +44,14 @@ class RANGON:
         self.body_vertical = pygame.image.load('assets/body_vertical.png').convert_alpha()
         self.body_horizontal = pygame.image.load('assets/body_horizontal.png').convert_alpha()
 
-        self.body_up = pygame.image.load('assets/move_up.png').convert_alpha()
-        self.body_down = pygame.image.load('assets/move_down.png').convert_alpha()
-        self.body_left = pygame.image.load('assets/move_left.png').convert_alpha()
-        self.body_right = pygame.image.load('assets/move_right.png').convert_alpha()
+        self.body_tr = pygame.image.load('assets/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('assets/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('assets/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('assets/body_bl.png').convert_alpha()
 
     def gambar_rangon(self):
         self.update_head_graphics()
-
+        self.update_tail_graphics()
         for index,block in enumerate(self.body):
             x_pos = int(block.x * ukuran_cell)
             y_pos = int(block.y * ukuran_cell)
@@ -59,14 +59,46 @@ class RANGON:
 
             if index == 0:
                 screen.blit(self.head,block_rect)
-            else:
-                pygame.draw.rect(screen, (72, 150, 189), block_rect)
+
+            elif index == len(self.body) -1:
+                screen.blit(self.tail, block_rect)
+            else: 
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+
+                    # sudut:
+                    # tl == top left/ atas kiri
+                else: 
+
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl, block_rect)
+           
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr, block_rect)
+           
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br, block_rect)
+           
     def update_head_graphics(self):
+
         head_relation = self.body[1] - self.body[0]
         if head_relation == Vector2(1, 0): self.head = self.head_left
         elif head_relation == Vector2(-1, 0): self.head = self.head_right
         elif head_relation == Vector2(0, 1): self.head = self.head_up
         elif head_relation == Vector2(0, -1): self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0, -1): self.tail = self.tail_down
     # menggerakan rangon
     def move_rangon(self):
 
@@ -115,9 +147,9 @@ class MAIN:
 
 # DISPLAY
 pygame.init()
-ukuran_cell = 26
-no_cell = 26
-screen = pygame.display.set_mode((no_cell * ukuran_cell, no_cell * ukuran_cell))
+ukuran_cell = 35
+no_cell = 20
+screen = pygame.display.set_mode((700, 700))
 pygame.display.set_caption('RANGON')
 clock = pygame.time.Clock()
 food = pygame.image.load('assets/food.png').convert_alpha()
